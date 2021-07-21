65b914c5e9e2f6d5214edde9a2b08ecced09fe3f
ABojo <alex.bojorquez2598@gmail.com>
2021-07-20 22:32:58 -0400

    removed router as I only need two routes/controller functions and it makes more sense to just add them to the app file as there isnt much clutter and it doesnt negatively impact readability

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
+const ytdlCore = require('ytdl-core');
+const fs = require('fs');
 const app = express();
 
 const apiRouter = require('./routes/apiRoutes');
@@ -8,8 +10,36 @@ const apiRouter = require('./routes/apiRoutes');
 //Request logging
 app.use(morgan('dev'));
 
+//parses post data and adds obj to req.body
+app.use(express.json());
+
 //Serve react build folder
 app.use(express.static(path.resolve('..', 'client', 'build')));
+
+app.post('/api/convert', async (req, res) => {
+  const { url } = req.body;
+
+  const readableStream = ytdlCore(url, { filter: 'audioonly' });
+  const info = await ytdlCore.getInfo(url);
+  const fileName = `${encodeURIComponent(
+    info.videoDetails.title
+  )}-${Date.now()}-output.mp3`;
+
+  const writableStream = fs.createWriteStream(
+    path.resolve('..', 'uploads', fileName)
+  );
+
+  const stream = readableStream.pipe(writableStream);
+
+  stream.on('finish', () => {
+    res.json({
+      status: 'success',
+      data: {
+        downloadLink: `/download/${fileName}`,
+      },
+    });
+  });
+});
 
 //routing
 app.use('/api', apiRouter);
