0dac7108265e7869cc5fb3685ea1a4374eef4d8d
ABojo <alex.bojorquez2598@gmail.com>
2021-07-20 20:10:29 -0400

    setup middleware stack by adding logging middleware + serving static files + adding an api router

--- server/server.js
+++ server/server.js
@@ -1,5 +1,18 @@
 const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
 const app = express();
+
+const apiRouter = require('./routes/apiRoutes');
+
+//Request logging
+app.use(morgan('dev'));
+
+//Serve react build folder
+app.use(express.static(path.resolve('..', 'client', 'build')));
+
+//routing
+app.use('/api', apiRouter);
 
 app.listen(process.env.PORT || 8080, () => {
   console.log('The server is now listening for incoming requests!');
