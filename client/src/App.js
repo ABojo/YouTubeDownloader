2f5558da40145e4c6118dd0d800c4993db99cb6e
ABojo <alex.bojorquez2598@gmail.com>
2021-07-22 17:40:25 -0400

    changed margins on header component + added state variables for holding the value in the input search field and the details of the selected video

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
 import Header from './components/Header';
 import SearchBox from './components/SearchBox';
+import { useState } from 'react';
 
 function App() {
+  const [searchValue, setSearchValue] = useState('');
+  const [videoDetails, setVideoDetails] = useState(null);
+
   return (
     <div className="max-w-2xl mx-auto w-11/12 py-12">
       <Header />
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,6 @@
 function Header() {
   return (
-    <div className="mb-12">
+    <div className="mb-6">
       <div className="text-red-500 flex items-center justify-center mb-3">
         <i className="fab fa-youtube text-6xl mr-3"></i>
         <h1 className="text-4xl">Downloader</h1>
