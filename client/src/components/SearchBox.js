9ce096fb17ed02c8cd7b733cc529444e2250ebec
ABojo <alex.bojorquez2598@gmail.com>
2021-07-23 16:03:18 -0400

    made styling updates + added a state variable for keeping track of what format the user is looking to download

--- client/src/components/SearchBox.js
+++ client/src/components/SearchBox.js
@@ -1,5 +1,7 @@
-function SearchBox(props) {
-  const { serachValue, setSearchValue } = props;
+import { useState } from 'react';
+
+function SearchBox({ serachValue, setSearchValue }) {
+  const [format, setFormat] = useState('mp4');
 
   return (
     <div>
@@ -8,21 +10,27 @@ function SearchBox(props) {
         <input
           onChange={(e) => setSearchValue(e.target.value)}
           value={serachValue}
-          className="w-full border-2 border-red-100 shadow-inner rounded-lg py-5 pl-8 pr-28"
+          className="w-full border-2 border-red-100 shadow-inner rounded-lg py-5 pl-10 pr-28"
           placeholder="Enter video URL here"
         ></input>
-        <button className="p-3 bg-red-500 hover:bg-red-400 font-bold text-white rounded-lg absolute right-2 top-1/2 transform -translate-y-1/2 transition duration-200 shadow">
+        <button
+          className="p-3 bg-red-500 hover:bg-red-400 font-bold text-white rounded-lg absolute right-2 top-1/2 transform -translate-y-1/2 transition duration-200 shadow"
+          onClick={() => console.log(format)}
+        >
           <i class="fas fa-download mr-1"></i> Download
         </button>
       </div>
       <div className="text-sm text-gray-500 flex justify-start">
-        <label class="container max-w-max mr-6 flex items-center">
+        <label
+          className="container max-w-max mr-6 flex items-center"
+          onClick={() => setFormat('mp4')}
+        >
           Video and Audio
           <input type="radio" defaultChecked={true} name="format" />
           <span class="checkmark"></span>
         </label>
 
-        <label class="container max-w-max">
+        <label className="container max-w-max" onClick={() => setFormat('mp3')}>
           <p className="text-md">Audio Only</p>
           <input type="radio" name="format" />
           <span class="checkmark"></span>
