681086013bc24f476aae2663e98d7ec465832717
ABojo <alex.bojorquez2598@gmail.com>
2021-07-24 17:37:14 -0400

    removed the load callback as its only really useful if I have multiple functions that I need that logic to wrap around. I only need it to wrap the getVideoDetails func so I added the logic straight to that function as it wont be used elsewhere + dispaly an error message if the user tries to download with an empty input

--- client/src/App.js
+++ client/src/App.js
@@ -13,24 +13,30 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState(null);
 
-  const loadCallback = (cb) => {
-    return async () => {
-      setIsLoading(true);
-      await cb();
-      setIsLoading(false);
-    };
-  };
+  const getVideoDetails = async () => {
+    //If the user hasnt entered a value into the input then display an error message
+    if (!searchValue)
+      return setErrorMessage('Sorry, you must enter a valid URL!');
 
-  const getVideoDetails = loadCallback(async () => {
+    //Clear error message everytime the user makes a request
+    setErrorMessage(null);
+
+    //Start loading
+    setIsLoading(true);
+
     const json = await API.getVideoDetails(searchValue, format);
+
+    //Stop loading
+    setIsLoading(false);
 
     if (json.status === 'error')
       return setErrorMessage(
         "Sorry, we couldn't find that video! Please make sure you entered the right URL."
       );
 
+    //If there was no error save the video details in state
     setVideoDetails(json.data);
-  });
+  };
 
   const clearVideoDetails = () => {
     setVideoDetails(null);
