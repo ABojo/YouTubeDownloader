75da1475ac28e3cebe1d9e2e753687fe1c5fa4f3
ABojo <alex.bojorquez2598@gmail.com>
2021-07-24 18:15:58 -0400

    was destructuring the wrong property name for the searchValue

--- client/src/components/SearchBox.js
+++ client/src/components/SearchBox.js
@@ -1,5 +1,5 @@
 function SearchBox({
-  serachValue,
+  searchValue,
   setSearchValue,
   setFormat,
   getVideoDetails,
@@ -10,7 +10,7 @@ function SearchBox({
         <i className="text-red-300 text-lg fas fa-link absolute left-2 top-1/2 transform -translate-y-1/2"></i>
         <input
           onChange={(e) => setSearchValue(e.target.value)}
-          value={serachValue}
+          value={searchValue}
           className="w-full border-2 border-red-100 shadow-inner rounded-lg py-5 pl-10 pr-36"
           placeholder="Enter video URL here"
         ></input>
