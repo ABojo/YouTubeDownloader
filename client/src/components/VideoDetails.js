cc74b1cbe17ad96d0b3ac3acd9f423a0711e3626
ABojo <alex.bojorquez2598@gmail.com>
2021-07-25 19:18:57 -0400

    made some changes to the mark up of the video details component

--- client/src/components/VideoDetails.js
+++ client/src/components/VideoDetails.js
@@ -9,8 +9,14 @@ function VideoDetails({ details, clearVideoDetails }) {
         src={details.embedUrl}
       ></iframe>
       <div className="mb-6">
-        <h1 className="text-lg font-bold text-gray-900">{details.title}</h1>
-        <h2 className="text-md text-gray-500 mb-3">{details.author}</h2>
+        <h1 className="text-lg font-bold text-gray-900">
+          <i class="fab fa-youtube-square mr-1"></i>
+          {details.title}
+        </h1>
+        <h2 className="text-md text-gray-500 mb-3">
+          <i class="far fa-user mr-1"></i>
+          {details.author}
+        </h2>
         <div className="text-sm text-gray-500 flex items-center">
           <p className="mr-3">
             <i className="fas fa-eye mr-1 text-yellow-500"></i>
@@ -26,23 +32,30 @@ function VideoDetails({ details, clearVideoDetails }) {
           </p>
         </div>
       </div>
-      <a
-        href={`${downloadPath}/mp4`}
-        className="w-full bg-red-500 rounded-lg p-3 text-white font-bold text-xl flex justify-center items-center mb-3 hover:bg-red-400 transition duration-200"
-        download
-      >
-        <i className="fas fa-video mr-3"></i> Download Video
-      </a>
-      <a
-        href={`${downloadPath}/mp3`}
-        className="w-full bg-red-500 rounded-lg p-3 text-white font-bold text-xl flex justify-center items-center mb-3 hover:bg-red-400 transition duration-200"
-        download
-      >
-        <i className="fas fa-music mr-3"></i> Download Audio
-      </a>
+      <div className="mb-6">
+        <a
+          href={`${downloadPath}/mp4`}
+          className="w-full bg-red-500 rounded-lg p-3 text-white font-bold text-lg flex justify-center items-center mb-3 hover:bg-red-400 transition duration-200 mr-6"
+          download
+        >
+          <i className="fas fa-video mr-3"></i> Download Video
+        </a>
+        <a
+          href={`${downloadPath}/mp3`}
+          className="w-full bg-red-500 rounded-lg p-3 text-white font-bold text-lg flex justify-center items-center mb-3 hover:bg-red-400 transition duration-200"
+          download
+        >
+          <i className="fas fa-music mr-3"></i> Download Audio
+        </a>
+      </div>
+      <div className="text-center mb-6 text-gray-500 uppercase text-sm flex justify-between items-center">
+        <div className="border-b border-gray-500 w-full"></div>
+        <p className="mx-3">Or</p>
+        <div className="border-b border-gray-500 w-full"></div>
+      </div>
       <button
         onClick={clearVideoDetails}
-        className="w-full border-gray-500 border rounded-lg p-3 text-gray-500 text-xl hover:bg-gray-200 transition duration-200"
+        className="w-full border-gray-500 border rounded-lg p-3 text-gray-500 text-md hover:bg-gray-200 transition duration-200"
       >
         Go Back
       </button>
