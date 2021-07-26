b1c07c679eaf278a6571dff5c2f748d0c747354f
ABojo <alex.bojorquez2598@gmail.com>
2021-07-26 15:52:34 -0400

    added an onKeypress handler so if the user hits enter while in the search bar it will try to fetch the details for the video

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <div className="flex justify-center">
       <a
-        className="text-4xl text-gray-900"
+        className="text-4xl text-gray-900 hover:text-gray-600 transition duration-200"
         href="https://github.com/ABojo/YouTubeDownloader"
         target="_blank"
         rel="noreferrer"
--- client/src/components/SearchBox.js
+++ client/src/components/SearchBox.js
@@ -4,6 +4,9 @@ function SearchBox({ videoId, setVideoId, getVideoDetails }) {
       <i className="text-red-300 text-lg fas fa-link absolute left-2 top-1/2 transform -translate-y-1/2"></i>
       <input
         onChange={(e) => setVideoId(e.target.value)}
+        onKeyPress={(e) => {
+          if (e.key === 'Enter') getVideoDetails();
+        }}
         value={videoId}
         className="w-full border-2 border-red-100 shadow-inner rounded-lg py-5 pl-10 pr-36"
         placeholder="Video URL or video ID"
