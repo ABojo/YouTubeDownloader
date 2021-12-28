4baa9bac2e9291692137b37e37dfa68bcc0a4d06
ABojo <alex.bojorquez2598@gmail.com>
2021-12-27 19:58:17 -0500

    error testing

--- server/server.js
+++ server/server.js
@@ -72,7 +72,7 @@ app.get('/api/videos/:videoId/:format', async (req, res, next) => {
 app.use((err, req, res, next) => {
   res.json({
     status: 'error',
-    data: err,
+    data: `${err}`,
     message: err.message,
   });
 });
