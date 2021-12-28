a28dc7b8fba1851e85425e1886fe68ce899b5328
ABojo <alex.bojorquez2598@gmail.com>
2021-12-28 01:12:50 -0500

    added visual clues to let the user know whether or not their input is valid and theyre now able to make a search

--- client/src/components/SearchBox.js
+++ client/src/components/SearchBox.js
@@ -6,21 +6,29 @@ function SearchBox({ videoId, setVideoId, getVideoDetails }) {
     window.scrollTo(0, 0);
   }, []);
 
+  const currentColor = videoId ? '-red-500' : '-red-100';
+  const transClasses = 'transition duration-300';
+
   return (
-    <div className="relative mb-24">
-      <i className="text-red-300 text-lg fas fa-link absolute left-2 top-1/2 transform -translate-y-1/2"></i>
+    <div className="relative mb-24 ">
+      <i
+        className={`text${currentColor} text-lg fas fa-link absolute left-2 top-1/2 transform -translate-y-1/2 ${transClasses}`}
+      ></i>
       <input
         onChange={(e) => setVideoId(e.target.value)}
         onKeyPress={(e) => {
           if (e.key === 'Enter') getVideoDetails();
         }}
         value={videoId}
-        className="w-full border-2 border-red-100 shadow-inner rounded-lg py-5 pl-10 pr-36"
-        placeholder="Video URL or video ID"
+        className={`w-full border-2  shadow-inner rounded-lg py-5 pl-10 pr-36 outline-none border${currentColor} ${transClasses}`}
+        placeholder="Enter a YouTube video URL or ID here"
       ></input>
       <button
+        disabled={!videoId}
         onClick={getVideoDetails}
-        className="p-3 bg-red-500 hover:bg-red-400 font-bold text-white rounded-lg absolute right-2 top-1/2 transform -translate-y-1/2 transition duration-200 shadow"
+        className={`p-3 bg${currentColor} ${
+          videoId ? 'hover:bg-red-400 shadow' : 'cursor-default'
+        } font-bold text-white rounded-lg absolute right-2 top-1/2 transform -translate-y-1/2 ${transClasses}`}
       >
         <i className="fas fa-search mr-1"></i> Search
       </button>
