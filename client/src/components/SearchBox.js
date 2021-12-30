aa25dffeeb452efe25ba039e9f06d86b4016fddd
ABojo <alex.bojorquez2598@gmail.com>
2021-12-29 23:58:39 -0500

    added a bg color to the search bar to make it stand out more as its own independent component + lowered the bottom margin to make the spacing between all elements more consistent

--- client/src/components/SearchBox.js
+++ client/src/components/SearchBox.js
@@ -10,28 +10,30 @@ function SearchBox({ videoId, setVideoId, getVideoDetails }) {
   const transClasses = 'transition duration-300';
 
   return (
-    <div className="relative mb-24 ">
-      <i
-        className={`text${currentColor} text-lg fas fa-link absolute left-2 top-1/2 transform -translate-y-1/2 ${transClasses}`}
-      ></i>
-      <input
-        onChange={(e) => setVideoId(e.target.value)}
-        onKeyPress={(e) => {
-          if (e.key === 'Enter' && videoId) getVideoDetails();
-        }}
-        value={videoId}
-        className={`w-full border-2  shadow-inner rounded-lg py-5 pl-10 pr-36 outline-none border${currentColor} ${transClasses}`}
-        placeholder="Enter a YouTube video URL or ID here"
-      ></input>
-      <button
-        disabled={!videoId}
-        onClick={getVideoDetails}
-        className={`p-3 bg${currentColor} ${
-          videoId ? 'hover:bg-red-400 shadow' : 'cursor-default'
-        } font-bold text-white rounded-lg absolute right-2 top-1/2 transform -translate-y-1/2 ${transClasses}`}
-      >
-        <i className="fas fa-search mr-1"></i> Search
-      </button>
+    <div className="mb-6 bg-gray-100 p-5 rounded-lg">
+      <div className="relative">
+        <i
+          className={`text${currentColor} text-lg fas fa-link absolute left-2 top-1/2 transform -translate-y-1/2 ${transClasses}`}
+        ></i>
+        <input
+          onChange={(e) => setVideoId(e.target.value)}
+          onKeyPress={(e) => {
+            if (e.key === 'Enter' && videoId) getVideoDetails();
+          }}
+          value={videoId}
+          className={`w-full border-2  shadow-inner rounded-lg py-5 pl-10 pr-36 outline-none border${currentColor} ${transClasses}`}
+          placeholder="Enter a YouTube video URL or ID here"
+        ></input>
+        <button
+          disabled={!videoId}
+          onClick={getVideoDetails}
+          className={`p-3 bg${currentColor} ${
+            videoId ? 'hover:bg-red-400 shadow' : 'cursor-default'
+          } font-bold text-white rounded-lg absolute right-2 top-1/2 transform -translate-y-1/2 ${transClasses}`}
+        >
+          <i className="fas fa-search mr-1"></i> Search
+        </button>
+      </div>{' '}
     </div>
   );
 }
