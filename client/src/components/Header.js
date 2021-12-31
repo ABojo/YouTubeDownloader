781b4707f1c0d055d85729485e6174e93881f554
ABojo <alex.bojorquez2598@gmail.com>
2021-12-30 22:22:09 -0500

    removed subheading in header + rebuilt client

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,9 +5,6 @@ function Header() {
         <i className="fab fa-youtube text-6xl mr-3"></i>
         <h1 className="text-4xl">Downloader</h1>
       </div>
-      <p className="text-md text-center mb-3 text-gray-500">
-        Download your favorite YouTube videos
-      </p>
     </div>
   );
 }
